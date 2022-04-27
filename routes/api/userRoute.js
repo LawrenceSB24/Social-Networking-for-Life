2ed43be3d423e18d0ded33b2e99211800367c0e1
LawrenceSB24 <lawrs2022@gmail.com>
2022-04-26 23:40:20 -0400

    Started work for user route

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,2 +1,22 @@
 // User GET, POST, PUT, and DELETE route
 // BONUS: Delete thoughts with said user
+
+const router = require('express').Router();
+
+// Const for importing user action functions from controller
+const {
+    getUsers,
+    getSoloUser,
+    createUser,
+    updateUser,
+    deleteUser
+
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSoloUser).delete(deleteUser);
+
+// 
