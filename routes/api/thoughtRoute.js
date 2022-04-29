43d8cb4479f339c112833d6afffb49869b32714e
LawrenceSB24 <lawrs2022@gmail.com>
2022-04-29 17:30:07 -0400

    Added single lines for app POST, PUT, and DELETE functionality

--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,1 +1,36 @@
 // Thoughts GET, POST, PUT, and DELETE route
+// const to import router express module
+
+const router = require('express').Router();
+// const for retrieving user thought modules
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+
+// /api/thoughts/thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
+
+// **DISCLAIMER** These routes are rudimentary and will be updated based on
+// user thought functionality.
+
+// example data
+// {
+//   "thoughtText": "Here's a cool thought...",
+//   "username": "lernantino",
+//   "userId": "5edff358a0fcb779aa7b118b" 
+// }
+
+module.exports = router;
--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -3,20 +3,33 @@
 
 const router = require('express').Router();
 
-// Const for importing user action functions from controller
+// const for retrieving modules for user info
 const {
     getUsers,
-    getSoloUser,
+    getSingleUser,
     createUser,
+    deleteUser,
     updateUser,
-    deleteUser
-
+    addFriend,
+    removeFriend
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSoloUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-// 
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/friendId').post(addFriend).delete(removeFriend);
+
+// **DISCLAIMER** These routes are rudimentary and will be updated based on
+// User functionality.
+
+// example data
+// {
+//  "username": "lernantino",
+//  "email": "lernantino@gmail.com"
+// }
+
+module.exports = router;
