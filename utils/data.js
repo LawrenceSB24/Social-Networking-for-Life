41e4cc3030099b1a2ac7c50586a67f9cc6bc53f2
LawrenceSB24 <lawrs2022@gmail.com>
2022-05-01 00:47:36 -0400

    Added comment for syntax of creating new thoughts

--- utils/data.js
+++ utils/data.js
@@ -51,4 +51,9 @@
 // //     "email": "lernantino@gmail.com"
 // //   }
 
-// // Example data for 
+// // Example data for thoughts
+// {
+//     "thoughtText": "Here's a cool thought...",
+//     "username": "lernantino",
+//     "userId": "5edff358a0fcb779aa7b118b"
+//   }
