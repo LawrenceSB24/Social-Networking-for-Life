e3bf2fa3f970dd2879fd967328bb567ca6af55a0
LawrenceSB24 <lawrs2022@gmail.com>
2022-04-30 20:13:57 -0400

    Added api intialization

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
 // Initialize file for api routes
+
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) = res.send('Wrong path in the collective. Turn back now!'));
+
+module.exports = router;
