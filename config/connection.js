9681c65cb8c92ee3ab62304121ea06789c73376b
LawrenceSB24 <lawrs2022@gmail.com>
2022-04-30 22:20:54 -0400

    Fixed formatting for database mongodb name

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@
 const {connect, connection} = require('mongoose');
 
 const connectionString = 
-    process.env.MONGODB_URI || 'mongodb://localhost:27017//usersDB';
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/social-networkDB';
 
 connect(connectionString, {
     useNewUrlParser: true,
