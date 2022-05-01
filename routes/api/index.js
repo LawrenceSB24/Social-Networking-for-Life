7762273bb0eba311b8aa6b957c6cf9f4d674c8f3
LawrenceSB24 <lawrs2022@gmail.com>
2022-04-30 20:13:07 -0400

    Added api route functionality initialization

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,11 @@
 // Initialization file for user and thought routes
+
+const router = require('express').Router();
+const userRoute = require('./userRoute');
+const thoughtRoute = require('./thoughtRoute');
+
+// Use of routes outside of routes folder
+router.use('/users', userRoute);
+router.use('/thoughts', thoughtRoute);
+
+module.exports = router;
