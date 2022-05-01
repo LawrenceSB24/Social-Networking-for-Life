922ffb473bd81955185e31ce5eb7188fd23ac5e4
LawrenceSB24 <lawrs2022@gmail.com>
2022-04-30 22:10:08 -0400

    Returned connection to MongoDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 // Connection file to MongoDB database
 
-// const {connect, connection} = require('mongoose');
+const {connect, connection} = require('mongoose');
 
-// const connectionString = 
-//     process.env.MONGODB_URI || 'mongodb://localhost:27017//usersDB';
+const connectionString = 
+    process.env.MONGODB_URI || 'mongodb://localhost:27017//usersDB';
 
-// connect(connectionString, {
-//     newUserParser: true,
-//     useUnifiedTopology: true
-// });
+connect(connectionString, {
+    newUserParser: true,
+    useUnifiedTopology: true
+});
 
-// module.exports = connection;
+module.exports = connection;
